32b6cf117ac130945a5d1bda748bd3bef08cc277
Kevin Ewing <notmyuwmail@gmail.com>
2013-02-24 21:07:31 -0800

    Mock data

--- js/main.js
+++ js/main.js
@@ -1,7 +1,71 @@
 (function() {
 	window.WCF = {
 		Utils: {
-			chartMaker: {}
+			chartMaker: {},
+			fakeUser: {
+				authToken: '8df7g6as9as8fguh',
+				id: 'foo',
+				vanityName: 'John Doe',
+				games: [
+					'gameId123',
+					'gameId456',
+					'gameId789'
+				],
+				currentGame: 'gameId123',
+				settings: {
+					extraHard: false
+				}
+			},
+
+			fakeGame: {
+				players: [
+					'userId1:3',
+					'userId2:2',
+					'userId3:2',
+					'userId4:0'
+				],
+				rounds: [
+					{
+						albumA: 'albumId',
+						albumB: 'albumId2',
+						results: [
+							'userId1',
+							'userId2',
+							'%%%',
+							'userId3'
+						]
+					},
+					{
+						albumA: 'albumId',
+						albumB: 'albumId2',
+						results: [
+							'userId1',
+							'userId3',
+							'%%%',
+							'userId2'
+						]
+					},
+					{
+						albumA: 'albumId',
+						albumB: 'albumId2',
+						results: [
+							'userId1',
+							'%%%'
+						]
+					}
+				],
+				nextRound: {
+					albumA: 'albumId',
+					albumB: 'albumId2',
+					results: [
+						'%%%'
+					]					
+				},
+				settings: {
+					dateRange: [1994, 2013],
+					isPrivate: true
+				}
+			}
 		},
 
 		init: function() {
--- js/modelbox.js
+++ js/modelbox.js
@@ -1,9 +1,21 @@
 (function() {
 	window.Models = {
 	};
-	Models.User = Backbone.Model.extend({});
-	Models.Game = Backbone.Model.extend({});
-	Models.Round = Backbone.Model.extend({});
+	Models.User = Backbone.Model.extend({
+		initialize: function() {},
+	});
+	Models.Game = Backbone.Model.extend({
+		initialize: function() {},
+		unpack: function() {},
+		getRound: function() {},
+		playRound: function() {},
+		playSong: function() {},
+		compareAge: function() {},
+		endRound: function() {}
+	});
+	Models.Round = Backbone.Model.extend({
+		initialize: function() {},
+	});
 
 	window.Views = {
 	};
