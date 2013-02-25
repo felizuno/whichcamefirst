f0a2cc73c795389731647cc407ac89fc9bec4a10
Kevin Ewing <notmyuwmail@gmail.com>
2013-02-24 20:38:10 -0800

    Close buttons

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@
 		Utils: {
 			chartMaker: {}
 		},
+
 		init: function() {
+			//Close buttons
+			$('.close').click(function() {
+				$(this).closest('.panel').fadeOut('fast');
+			});
+
+			// Header behavior
 			$('.header').click(function() {
 				var $el = $(this);
-				
+
 				if ($el.hasClass('hiding')) {
 					$el.animate({left: '+=90%'});
 				} else {
