e03f5a59326620b35aee397567d170db05baea4c
Kevin Ewing <notmyuwmail@gmail.com>
2013-03-16 11:30:49 -0700

    Better model/view creation order

--- js/main.js
+++ js/main.js
@@ -2,16 +2,15 @@
 	window.WCF = {
 		Utils: {},
 		init: function() {
+			this.user = new Models.User(this.Utils.fakeUserConfig);
+			this.currentGame = new Models.Game(this.Utils.fakeGameConfig);
+
+			this.gameSelectView = new Views.GameSelectView({
+				el: $('#gametypechooser')
+			});
 			this.headerBar = new Views.HeaderBar({
 				el: $('.header')
 			});
-
-			this.roundSelectView = new Views.GameSelectView({
-				el: $('#gametypechooser')
-			});
-
-			this.user = new Models.User(this.Utils.fakeUserConfig);
-			this.currentGame = new Models.Game(this.Utils.fakeGameConfig);
 		}
 	};
 
